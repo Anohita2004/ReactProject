62d23af361514e73959ece30856939fb21981a55
Anohita <anohitamukherjee@gmail.com>
2025-08-27 13:19:36 +0530

    Initial commit

---
+++ src/Greet.jsx
@@ -0,0 +1,4 @@
+function Greet(){
+    return <h1>GREETINGS TO YOU!</h1>
+}
+export default Greet;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return(
+        <main>
+            <h2>"Main Content"</h2>   
+            <p>This is my webpage</p>  
+                   </main>
+    )
+}
+export default MainContent;
