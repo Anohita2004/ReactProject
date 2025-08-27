2fc51f551589c7bf66c9b8c3d7d2a711bc0cb556
Anohita <anohitamukherjee@gmail.com>
2025-08-27 13:32:30 +0530

    Welcome Message displayed

---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,8 @@
+function WelcomeMessage(){
+    return(
+        <div>
+            <h1>Welcome to JSX Learning!</h1>
+            <p>This is the JSX learning page!</p>
+        </div>    )
+}
+export default WelcomeMessage;
